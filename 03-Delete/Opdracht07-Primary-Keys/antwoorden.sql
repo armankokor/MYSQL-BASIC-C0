940f9902451b7dc398d5ded1e9702449bd08c5a6
armankokor <2158154@talnet.nl>
2022-01-24 10:18:40 +0100

    opd7

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,3 +1,4 @@
-DELETE from `players` WHERE id = "3093309"
+DELETE from `players` WHERE id = ""
 /*dit is de code om de dubbele rijen te verwijderen
 /* het lukte ook uiteindelijk om de primary key toe te kennen aan de id-kolom in PhpMyadmin
+/*ik ben de id vergeten
